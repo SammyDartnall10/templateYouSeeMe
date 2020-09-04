a9849fe8180bfb7b9fdf0ca5ba4fa56f1741d8d3
Sammy <samantha.megan10@gmail.com>
2020-09-04 01:18:36 -0400

    add react router dom

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import HelloWorld from './components/HelloWorld'
 import Counter from './components/Counter'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
